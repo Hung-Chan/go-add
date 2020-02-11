b4eab6b5c6bb2131e7990a8b8a0e6c8475c42a7f
阿fa <20190303@cyadmk.com>
2020-02-11 16:19:29 +0800

    init commit

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("start")
+	number1 := 1
+	number2 := 2
+
+	total := add(number1, number2)
+
+	fmt.Println(total)
+	fmt.Println("end")
+}
+
+// add .
+func add(num1, num2 int) int {
+	return num1 + num2
+}
